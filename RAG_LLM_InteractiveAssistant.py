dea6d51cab03812f76c473813fd2b7bb7d29c2bb
Md-AdeebKhan <22.607mdadeeb@gmail.com>
2025-11-11 20:43:19 +0530

    Fix import path for RecursiveCharacterTextSplitter

--- RAG_LLM_InteractiveAssistant.py
+++ RAG_LLM_InteractiveAssistant.py
@@ -1,7 +1,7 @@
 from langchain_groq import ChatGroq
 import os
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.memory import ConversationBufferMemory
